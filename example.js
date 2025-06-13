9d5f97ff4f8443f957949f7131ec706205d6a803
kavya <kalva6986@gmail.com>
2025-06-13 14:38:06 +0530

    main

---
+++ example.js
@@ -0,0 +1,36 @@
+/*console.log('nenu js nerchukuntunnanu')
+console.log('I will try my level best 😊')
+//alert("hi welcome")
+console.log(window)
+/*let kavyaamount = 2000;
+console.log(kavyaamount);
+let panduamount = 5000;
+console.log(panduamount);
+let totalamount = kavyaamount+panduamount;
+console.log(totalamount);
+prompt("Enter your name.")
+prompt("Enter your height in cm")
+prompt("Enter your weight in cm")*/
+/*const kavyaamount = 2000;
+console.log(kavyaamount);
+const panduamount = 5000;
+console.log(panduamount);
+const totalamount = kavyaamount+panduamount;
+console.log(totalamount);*/
+/*console.log("pandu")
+alert("Hii i'm paul")
+prompt("Enter your name")
+prompt("Enter your age")
+prompt("Enter you wife name")
+alert("Thank you")*/
+console.log('Hii, "kavya"')
+console.log("Very good morning")
+console.log('Nice to meet you')
+console.log("I'll try my level best practicing  js")
+console.log(`hi kavya`)
+console.log(window)
+var kavyaamount=300;
+var kavyaamount=400
+var paulamount=500;
+var totalamount=kavyaamount+paulamount;
+console.log(totalamount)
