1a2c301b905c95eec74b10d82f19f722e86a79fc
kavya <kalva6986@gmail.com>
2025-06-21 13:25:19 +0530

    project-3

--- example.js
+++ example.js
@@ -1,36 +1,31 @@
-/*console.log('nenu js nerchukuntunnanu')
-console.log('I will try my level best 😊')
-//alert("hi welcome")
-console.log(window)
-/*let kavyaamount = 2000;
-console.log(kavyaamount);
-let panduamount = 5000;
-console.log(panduamount);
-let totalamount = kavyaamount+panduamount;
-console.log(totalamount);
-prompt("Enter your name.")
-prompt("Enter your height in cm")
-prompt("Enter your weight in cm")*/
-/*const kavyaamount = 2000;
-console.log(kavyaamount);
-const panduamount = 5000;
-console.log(panduamount);
-const totalamount = kavyaamount+panduamount;
-console.log(totalamount);*/
-/*console.log("pandu")
-alert("Hii i'm paul")
-prompt("Enter your name")
-prompt("Enter your age")
-prompt("Enter you wife name")
-alert("Thank you")*/
-console.log('Hii, "kavya"')
-console.log("Very good morning")
-console.log('Nice to meet you')
-console.log("I'll try my level best practicing  js")
-console.log(`hi kavya`)
-console.log(window)
-var kavyaamount=300;
-var kavyaamount=400
-var paulamount=500;
-var totalamount=kavyaamount+paulamount;
-console.log(totalamount)
+const simsstudent = document.getElementById("simsstudent");
+const idcard = document.getElementById("idcard");
+const age18 = document.getElementById("age18");
+const checkButton = document.getElementById("check");
+const passengerstatus = document.getElementById("status");
+
+checkButton.addEventListener("click",function(){
+    let issimsstudent=simsstudent.checked;
+    let hasidcard=idcard.checked;
+    let isageabove18=age18.checked;
+    console.log(issimsstudent,hasidcard,isageabove18);
+    if(issimsstudent){
+        console.log("seat is available for you😊");
+        if(hasidcard){
+            console.log("Has id card");
+            if(isageabove18){
+                console.log("Aged above 18");
+                alert("please come inside");
+            }else{
+                console.log("but your age is below 18. ");
+                passengerstatus.textContent="But your age is below 18";
+            }
+        }else{
+            console.log("but you don't have an id card");
+            passengerstatus.textContent="But you don't have an id card"
+        }
+    }else{
+        console.log("you are not a sims student.");
+        passengerstatus.textContent="you are not a sims student."
+    }
+})
